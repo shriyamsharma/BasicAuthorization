a66a5ca7bd33bacea4d4cd701aa14c3fabf6c872
shriyamsharma <sharmashriyam676@gmail.com>
2020-03-14 18:29:41 +0530

    Express REST API with Mongoose Part One

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -1,23 +1,43 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Dishes = require('../models/dishes');
 
 const dishRouter = express.Router();
 
 dishRouter.use(bodyParser.json());
 
 dishRouter.route('/')
-.all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
+// .all((req, res, next) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     next();
+// })
 
 .get((req, res, next) => {
-    res.end('will send all the dishes to you!');
+    // res.end('will send all the dishes to you!');
+
+    Dishes.find({})
+    .then((dishes) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dishes);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
 
 .post((req, res, next) => {
-    res.end('Will add the dishe: ' + req.body.name + ' with the details: ' + req.body.description);
+    // res.end('Will add the dishe: ' + req.body.name + ' with the details: ' + req.body.description);
+
+    Dishes.create(req.body)
+    .then((dish) => {
+        console.log('Dish Created ', dish);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dish);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
 
 .put((req, res, next) => {
@@ -26,7 +46,15 @@ dishRouter.route('/')
 })
 
 .delete((req, res, next) => {
-    res.end('Deleting all the dishes.');
+    // res.end('Deleting all the dishes.');
+
+    Dishes.remove({})
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    },  (err) => next(err))
+    .catch((err) => next(err));
 });
 
 module.exports = dishRouter;
