ebb92bd4657309345ce5cf97365503ec60c915df
shriyamsharma <sharmashriyam676@gmail.com>
2020-03-16 01:11:22 +0530

    Basic Authentication

--- app.js
+++ app.js
@@ -27,10 +27,71 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+app.use(logger('dev')); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+function auth (req, res, next) {
+  console.log(req.headers);
+  var authHeader = req.headers.authorization;
+  if (!authHeader) {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      next(err);
+      return;
+  }
+
+  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  var user = auth[0];
+  var pass = auth[1];
+  if (user == 'admin' && pass == 'password') {
+      next(); // authorized
+  } else {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');      
+      err.status = 401;
+      next(err);
+  }
+}
+
+app.use(auth);
+
+
+
+// function auth(req, res, next) {
+//   console.log(req.headers);
+
+//   var authHeader = req.headers.authorization;
+
+//   if (!authHeader) {
+//     var err = new Error('You are not authenticated');
+
+//     res.setHeader('WWW-Authenticate', 'Basic');
+//     err.status = 401;
+//     return next(err);
+//   }
+
+//   var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString.split(':');
+ 
+//   var username = auth[0];
+//   var password = auth[1];
+
+//   if (username === 'admin' && password === 'password') {
+//     next();
+//   } else {
+//     var err = new Error('You are not authenticated');
+
+//     res.setHeader('WWW-Authenticate', 'Basic');
+//     err.status = 401;
+//     return next(err);
+//   }
+
+// }
+
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
